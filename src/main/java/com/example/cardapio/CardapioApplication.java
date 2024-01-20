2b99c9d60deeb03398f4477edf88c12de4f281b3
Thiago Augusto de Souza Moura <77154719+ThiagoAug@users.noreply.github.com>
2024-01-20 16:25:00 -0300

    alteracao de valores
    Alterando valores

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CardapioApplication {
 
 	public static void main(String[] args) {
-		System.out.println("\n\n\n\n\nINICIANDO... INICIANDO... INICIANDO...\n\n\n\n\n");
 		SpringApplication.run(CardapioApplication.class, args);
-		System.out.println("\n\n\n\n\nFUNCIONANDO... FUNCIONANDO... FUNCIONANDO...\n\n\n\n\n");
 	}
 
 }
